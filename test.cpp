259c81636ee93fb2620284dcdda88e9979143aa6
Aquila <aquila-2004@yandex.ru>
2017-12-17 11:43:23 +0300

    Удалено использование string literal (travis не поддерживает)

--- test.cpp
+++ test.cpp
@@ -7,8 +7,6 @@
 #include "lib.h"
 #include "split_ip.h"
 
-using namespace std::string_literals;
-
 BOOST_AUTO_TEST_SUITE( test_ip_filter )
 
 BOOST_AUTO_TEST_CASE( test_valid_version )
@@ -20,23 +18,23 @@ BOOST_AUTO_TEST_CASE( test_split_ip1 )
 {
     // ("",  '.') -> [""]
     auto res = split( "", '.' );
-    BOOST_REQUIRE_EQUAL( res.size(), (size_t)1 );
+    BOOST_REQUIRE_EQUAL( res.size(), size_t(1) );
     BOOST_REQUIRE( res.at( 0 ).empty() );
 }
 
 BOOST_AUTO_TEST_CASE( test_split_ip2 )
 {
     // ("11", '.') -> ["11"]
-    auto res = split( "11"s, '.' );
-    BOOST_REQUIRE_EQUAL( res.size(), (size_t)1 );
-    BOOST_REQUIRE_EQUAL( res.at( 0 ), "11"s );
+    auto res = split( "11", '.' );
+    BOOST_REQUIRE_EQUAL( res.size(), size_t(1) );
+    BOOST_REQUIRE_EQUAL( res.at( 0 ), "11" );
 }
 
 BOOST_AUTO_TEST_CASE( test_split_ip3 )
 {
     // ("..", '.') -> ["", "", ""]
-    auto etalon = { ""s, ""s, ""s };
-    auto res = split( ".."s, '.' );
+    auto etalon = { "", "", "" };
+    auto res = split( "..", '.' );
     BOOST_REQUIRE( res.size() == 3 );
     BOOST_REQUIRE_EQUAL_COLLECTIONS( res.begin(), res.end(), etalon.begin(), etalon.end() );
 }
@@ -44,8 +42,8 @@ BOOST_AUTO_TEST_CASE( test_split_ip3 )
 BOOST_AUTO_TEST_CASE( test_split_ip4 )
 {
     // ("11.", '.') -> ["11", ""]
-    auto etalon = { "11"s, ""s };
-    auto res = split( "11."s, '.' );
+    auto etalon = { "11", "" };
+    auto res = split( "11.", '.' );
     BOOST_REQUIRE( res.size() == 2 );
     BOOST_REQUIRE_EQUAL_COLLECTIONS( res.begin(), res.end(), etalon.begin(), etalon.end() );
 }
@@ -53,8 +51,8 @@ BOOST_AUTO_TEST_CASE( test_split_ip4 )
 BOOST_AUTO_TEST_CASE( test_split_ip5 )
 {
     // (".11", '.') -> ["", "11"]
-    auto etalon = { ""s, "11"s };
-    auto res = split( ".11"s, '.' );
+    auto etalon = { "", "11" };
+    auto res = split( ".11", '.' );
     BOOST_REQUIRE( res.size() == 2 );
     BOOST_REQUIRE_EQUAL_COLLECTIONS( res.begin(), res.end(), etalon.begin(), etalon.end() );
 }
@@ -62,8 +60,8 @@ BOOST_AUTO_TEST_CASE( test_split_ip5 )
 BOOST_AUTO_TEST_CASE( test_split_ip6 )
 {
     // ("11.22", '.') -> ["11", "22"]
-    auto etalon = { "11"s, "22"s };
-    auto res = split( "11.22"s, '.' );
+    auto etalon = { "11", "22" };
+    auto res = split( "11.22", '.' );
     BOOST_REQUIRE( res.size() == 2 );
     BOOST_REQUIRE_EQUAL_COLLECTIONS( res.begin(), res.end(), etalon.begin(), etalon.end() );
 }
