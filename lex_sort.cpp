00cce7743d5acf66f097c7bc0779009822652ebc
Aquila <aquila-2004@yandex.ru>
2017-12-20 23:32:06 +0300

    Замена цикла на оператор сравнение в lex_sort

--- lex_sort.cpp
+++ lex_sort.cpp
@@ -8,21 +8,8 @@ void lexicographically_reverse_sort( ip_pool_t& ip_pool )
     std::sort( std::begin( ip_pool ), std::end( ip_pool ),
         []( const auto& left, const auto& right )
     {
-        if( left.size() != right.size() )
-            throw std::runtime_error( "incorect length ip" );
-
-        auto size = left.size();
-        for( decltype( size ) i = 0; i < size; ++i )
-        {
-            if( left[i] > right[i] )
-            {
-                return true;
-            }
-            else if( left[i] < right[i] )
-            {
-                return false;
-            }
-        }
+        if( left > right )
+            return true;
         return false;
     } );
 }
