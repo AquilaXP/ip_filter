381e20d1c7571e8ffcdb43677f47e6db9f2a3b6f
Aquila <aquila-2004@yandex.ru>
2017-12-31 11:05:23 +0300

    В lex_sort использован готовый компаратор std::greater

--- lex_sort.cpp
+++ lex_sort.cpp
@@ -1,15 +1,9 @@
 #include "lex_sort.h"
 
 #include <algorithm>
-#include <stdexcept>
+#include <functional>
 
 void lexicographically_reverse_sort( ip_pool_t& ip_pool )
 {
-    std::sort( std::begin( ip_pool ), std::end( ip_pool ),
-        []( const auto& left, const auto& right )
-    {
-        if( left > right )
-            return true;
-        return false;
-    } );
+    std::sort( std::begin( ip_pool ), std::end( ip_pool ), std::greater<>() );
 }
